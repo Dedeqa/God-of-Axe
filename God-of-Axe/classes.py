7cffeba78ededc9fe05ee274a4096ab7324b7c75
F3dosik <izhevsk2073@gmail.com>
2023-05-12 18:47:23 +0400

    new Tree()

--- God-of-Axe/classes.py
+++ God-of-Axe/classes.py
@@ -15,8 +15,8 @@ class Player(Unit, pygame.sprite.Sprite):
     def __init__(self, nm, hp, posx, posy):
         Unit.__init__(self, nm, hp, posx, posy)
         pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.Surface((50, 50))
-        self.image.fill('Black')
+        self.image = pygame.image.load('Images/minotaur-N-stand.png')
+
         self.rect = self.image.get_rect()
         self.rect.center = (posx / 2, posy / 2)
         self.weapon = Weapon(10)
@@ -30,24 +30,25 @@ class Player(Unit, pygame.sprite.Sprite):
         keystate = pygame.key.get_pressed()
         if not (keystate[pygame.K_a] and keystate[pygame.K_d]):
             if keystate[pygame.K_a]:
-                if self.rect[0] >= 50:
+                if self.rect.x >= 50:
                     self.speedx = -5
                 elif cfg.bg_x < 1920:
                     cfg.bg_x += 5
             if keystate[pygame.K_d]:
-                if self.rect[0] <= 1820:
+                if self.rect.x <= 1820:
                     self.speedx = 5
                 elif cfg.bg_x > -1920:
                     cfg.bg_x -= 5
                     print(cfg.bg_x)
+
         if not (keystate[pygame.K_w] and keystate[pygame.K_s]):
             if keystate[pygame.K_w]:
-                if self.rect[1] >= 50:
+                if self.rect.y >= 50:
                     self.speedy = -5
                 elif cfg.bg_y < 1080:
                     cfg.bg_y += 5
             if keystate[pygame.K_s]:
-                if self.rect[1] <= 980:
+                if self.rect.y <= 980:
                     self.speedy = 5
                 elif cfg.bg_y > -1080:
                     cfg.bg_y -= 5
@@ -70,3 +71,19 @@ class Weapon:
 all_sprites = pygame.sprite.Group()
 player = Player("Albert", 100, cfg.WIDTH, cfg.HEIGHT)
 all_sprites.add(player)
+#--------------------------------------------------------------------------------------------------
+
+class Tree(Unit, pygame.sprite.Sprite):
+    def __init__(self, nm, hp, posx, posy, bonus):
+        Unit.__init__(self, nm, hp, posx, posy)
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load("Images/Trees/Tree1.png")
+        self.rect = self.image.get_rect()
+        self.rect.center = (posx, posy)
+        self.bonus = bonus
+    def update(self):
+        self.rect.x = cfg.bg_x + self.posx
+        self.rect.y = cfg.bg_y + self.posy
+
+tree = Tree("Bereza", 300, 960, 800, 5)
+all_sprites.add(tree)
