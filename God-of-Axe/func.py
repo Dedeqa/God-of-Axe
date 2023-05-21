7d1214ae6b463ba676610be945e9589964ec03b9
F3dosik <izhevsk2073@gmail.com>
2023-05-21 22:06:48 +0400

    update()

--- God-of-Axe/func.py
+++ God-of-Axe/func.py
@@ -12,7 +12,7 @@ def start_game():
     if cfg.start_game_flag:
         # cfg.play_music.play(-1)
         cfg.start_game_flag = False
-    tree_generator(100)
+    tree_generator(500)
     while True:
         cfg.clock.tick(cfg.FPS)
         # print(cfg.clock.get_fps())
