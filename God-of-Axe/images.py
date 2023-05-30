fa926de544e46f0b16a9e6ffc056bd9f2bde2a8a
Hodor <hagberry_am@list.ru>
2023-05-30 23:19:53 +0400

    path images 100%

--- God-of-Axe/images.py
+++ God-of-Axe/images.py
@@ -152,7 +152,6 @@ woodcutter_run4_r_p = f.resource_path('Images/Player/Woodcutter_run_right/Woodcu
 woodcutter_run5_r_p = f.resource_path('Images/Player/Woodcutter_run_right/Woodcutter_run5.png')
 woodcutter_run6_r_p = f.resource_path('Images/Player/Woodcutter_run_right/Woodcutter_run6.png')
 
-
 woodcutter_run_right = [
     pygame.transform.scale(
         pygame.image.load(woodcutter_run1_r_p).convert_alpha(), (50, 50)),
@@ -175,7 +174,6 @@ woodcutter_run3_l_p = f.resource_path('Images/Player/Woodcutter_run_left/Woodcut
 woodcutter_run4_l_p = f.resource_path('Images/Player/Woodcutter_run_left/Woodcutter_run4.png')
 woodcutter_run5_l_p = f.resource_path('Images/Player/Woodcutter_run_left/Woodcutter_run5.png')
 woodcutter_run6_l_p = f.resource_path('Images/Player/Woodcutter_run_left/Woodcutter_run6.png')
-
 
 woodcutter_run_left = [
     pygame.transform.scale(
@@ -200,7 +198,6 @@ woodcutter_attack4_r_p = f.resource_path('Images/Player/Woodcutter_attack_right/
 woodcutter_attack5_r_p = f.resource_path('Images/Player/Woodcutter_attack_right/attack_5.png')
 woodcutter_attack6_r_p = f.resource_path('Images/Player/Woodcutter_attack_right/attack_6.png')
 
-
 woodcutter_attack_right = [
     pygame.transform.scale(
         pygame.image.load(woodcutter_attack1_r_p).convert_alpha(), (50, 50)),
@@ -217,94 +214,132 @@ woodcutter_attack_right = [
 ]
 
 # Атака влево ----------------------------------------------------------------------------------------------------------
+woodcutter_attack1_l_p = f.resource_path('Images/Player/Woodcutter_attack_left/attack_1.png')
+woodcutter_attack2_l_p = f.resource_path('Images/Player/Woodcutter_attack_left/attack_2.png')
+woodcutter_attack3_l_p = f.resource_path('Images/Player/Woodcutter_attack_left/attack_3.png')
+woodcutter_attack4_l_p = f.resource_path('Images/Player/Woodcutter_attack_left/attack_4.png')
+woodcutter_attack5_l_p = f.resource_path('Images/Player/Woodcutter_attack_left/attack_5.png')
+woodcutter_attack6_l_p = f.resource_path('Images/Player/Woodcutter_attack_left/attack_6.png')
+
 woodcutter_attack_left = [
     pygame.transform.scale(
-        pygame.image.load('Images/Player/Woodcutter_attack_left/attack_1.png').convert_alpha(), (50, 50)),
+        pygame.image.load(woodcutter_attack1_l_p).convert_alpha(), (50, 50)),
     pygame.transform.scale(
-        pygame.image.load('Images/Player/Woodcutter_attack_left/attack_2.png').convert_alpha(), (50, 50)),
+        pygame.image.load(woodcutter_attack2_l_p).convert_alpha(), (50, 50)),
     pygame.transform.scale(
-        pygame.image.load('Images/Player/Woodcutter_attack_left/attack_3.png').convert_alpha(), (50, 50)),
+        pygame.image.load(woodcutter_attack3_l_p).convert_alpha(), (50, 50)),
     pygame.transform.scale(
-        pygame.image.load('Images/Player/Woodcutter_attack_left/attack_4.png').convert_alpha(), (50, 50)),
+        pygame.image.load(woodcutter_attack4_l_p).convert_alpha(), (50, 50)),
     pygame.transform.scale(
-        pygame.image.load('Images/Player/Woodcutter_attack_left/attack_5.png').convert_alpha(), (50, 50)),
+        pygame.image.load(woodcutter_attack5_l_p).convert_alpha(), (50, 50)),
     pygame.transform.scale(
-        pygame.image.load('Images/Player/Woodcutter_attack_left/attack_6.png').convert_alpha(), (50, 50)),
+        pygame.image.load(woodcutter_attack6_l_p).convert_alpha(), (50, 50)),
 ]
 
 # ----------------------------------------------- Спрайты минотавра ----------------------------------------------------
 
-# Хотьба вниз ----------------------------------------------------------------------------------------------------------
+# Ходьба вниз ----------------------------------------------------------------------------------------------------------
+minotaur_walk_bottom1_p = f.resource_path('Images/monsters/walk_bottom/minotaur-S-stand.png')
+minotaur_walk_bottom2_p = f.resource_path('Images/monsters/walk_bottom/minotaur-S-step1.png')
+minotaur_walk_bottom3_p = f.resource_path('Images/monsters/walk_bottom/minotaur-S-step2.png')
+
 minotaur_walk_bottom = [
     pygame.transform.scale(
-        pygame.image.load('Images/monsters/walk_bottom/minotaur-S-stand.png').convert_alpha(), (50, 50)),
+        pygame.image.load(minotaur_walk_bottom1_p).convert_alpha(), (50, 50)),
     pygame.transform.scale(
-        pygame.image.load('Images/monsters/walk_bottom/minotaur-S-step1.png').convert_alpha(), (50, 50)),
+        pygame.image.load(minotaur_walk_bottom2_p).convert_alpha(), (50, 50)),
     pygame.transform.scale(
-        pygame.image.load('Images/monsters/walk_bottom/minotaur-S-step2.png').convert_alpha(), (50, 50))
+        pygame.image.load(minotaur_walk_bottom3_p).convert_alpha(), (50, 50))
 ]
 # Хотьба вверх ---------------------------------------------------------------------------------------------------------
+minotaur_walk_top1_p = f.resource_path('Images/monsters/walk_top/minotaur-N-stand.png')
+minotaur_walk_top2_p = f.resource_path('Images/monsters/walk_top/minotaur-N-step1.png')
+minotaur_walk_top3_p = f.resource_path('Images/monsters/walk_top/minotaur-N-step2.png')
+
 minotaur_walk_top = [
     pygame.transform.scale(
-        pygame.image.load('Images/monsters/walk_top/minotaur-N-stand.png').convert_alpha(), (50, 50)),
+        pygame.image.load(minotaur_walk_top1_p).convert_alpha(), (50, 50)),
     pygame.transform.scale(
-        pygame.image.load('Images/monsters/walk_top/minotaur-N-step1.png').convert_alpha(), (50, 50)),
+        pygame.image.load(minotaur_walk_top2_p).convert_alpha(), (50, 50)),
     pygame.transform.scale(
-        pygame.image.load('Images/monsters/walk_top/minotaur-N-step2.png').convert_alpha(), (50, 50))
+        pygame.image.load(minotaur_walk_top3_p).convert_alpha(), (50, 50))
 ]
 
-# Хотьба влево ---------------------------------------------------------------------------------------------------------
+# Ходьба влево ---------------------------------------------------------------------------------------------------------
+minotaur_walk_left1_p = f.resource_path('Images/monsters/walk_left/minotaur-W-stand.png')
+minotaur_walk_left2_p = f.resource_path('Images/monsters/walk_left/minotaur-W-step1.png')
+minotaur_walk_left3_p = f.resource_path('Images/monsters/walk_left/minotaur-W-step2.png')
+
 minotaur_walk_left = [
     pygame.transform.scale(
-        pygame.image.load('Images/monsters/walk_left/minotaur-W-stand.png').convert_alpha(), (50, 50)),
+        pygame.image.load(minotaur_walk_left1_p).convert_alpha(), (50, 50)),
     pygame.transform.scale(
-        pygame.image.load('Images/monsters/walk_left/minotaur-W-step1.png').convert_alpha(), (50, 50)),
+        pygame.image.load(minotaur_walk_left2_p).convert_alpha(), (50, 50)),
     pygame.transform.scale(
-        pygame.image.load('Images/monsters/walk_left/minotaur-W-step2.png').convert_alpha(), (50, 50))
+        pygame.image.load(minotaur_walk_left1_p).convert_alpha(), (50, 50))
 ]
 
 # Ходьба вправо --------------------------------------------------------------------------------------------------------
+minotaur_walk_right1_p = f.resource_path('Images/monsters/walk_right/minotaur_stand_right.png')
+minotaur_walk_right2_p = f.resource_path('Images/monsters/walk_right/minotaur_step1_right.png')
+minotaur_walk_right3_p = f.resource_path('Images/monsters/walk_right/minotaur_step2_right.png')
+
 minotaur_walk_right = [
     pygame.transform.scale(
-        pygame.image.load('Images/monsters/walk_right/minotaur_stand_right.png').convert_alpha(), (50, 50)),
+        pygame.image.load(minotaur_walk_right1_p).convert_alpha(), (50, 50)),
     pygame.transform.scale(
-        pygame.image.load('Images/monsters/walk_right/minotaur_step1_right.png').convert_alpha(), (50, 50)),
+        pygame.image.load(minotaur_walk_right2_p).convert_alpha(), (50, 50)),
     pygame.transform.scale(
-        pygame.image.load('Images/monsters/walk_right/minotaur_step2_right.png').convert_alpha(), (50, 50))
+        pygame.image.load(minotaur_walk_right3_p).convert_alpha(), (50, 50))
 ]
 # Получение урона Player -----------------------------------------------------------------------------------------------
+woodcutter_hurt_right1_p = f.resource_path('Images/Player/Woodcutter_hurt_right/Woodcutter_hurt1.png')
+woodcutter_hurt_right2_p = f.resource_path('Images/Player/Woodcutter_hurt_right/Woodcutter_hurt2.png')
+woodcutter_hurt_right3_p = f.resource_path('Images/Player/Woodcutter_hurt_right/Woodcutter_hurt3.png')
+
 woodcutter_hurt_right = [
     pygame.transform.scale(pygame.image.load(
-        'Images/Player/Woodcutter_hurt_right/Woodcutter_hurt1.png').convert_alpha(), (50, 50)),
+        woodcutter_hurt_right1_p).convert_alpha(), (50, 50)),
     pygame.transform.scale(pygame.image.load(
-        'Images/Player/Woodcutter_hurt_right/Woodcutter_hurt2.png').convert_alpha(), (50, 50)),
+        woodcutter_hurt_right2_p).convert_alpha(), (50, 50)),
     pygame.transform.scale(pygame.image.load(
-        'Images/Player/Woodcutter_hurt_right/Woodcutter_hurt3.png').convert_alpha(), (50, 50))
+        woodcutter_hurt_right3_p).convert_alpha(), (50, 50))
 ]
+
+woodcutter_hurt_left1_p = f.resource_path('Images/Player/Woodcutter_hurt_left/Woodcutter_hurt_left1.png')
+woodcutter_hurt_left2_p = f.resource_path('Images/Player/Woodcutter_hurt_left/Woodcutter_hurt_left2.png')
+woodcutter_hurt_left3_p = f.resource_path('Images/Player/Woodcutter_hurt_left/Woodcutter_hurt_left3.png')
+
 woodcutter_hurt_left = [
     pygame.transform.scale(pygame.image.load(
-        'Images/Player/Woodcutter_hurt_left/Woodcutter_hurt_left1.png').convert_alpha(), (50, 50)),
+        woodcutter_hurt_left1_p).convert_alpha(), (50, 50)),
     pygame.transform.scale(pygame.image.load(
-        'Images/Player/Woodcutter_hurt_left/Woodcutter_hurt_left2.png').convert_alpha(), (50, 50)),
+        woodcutter_hurt_left2_p).convert_alpha(), (50, 50)),
     pygame.transform.scale(pygame.image.load(
-        'Images/Player/Woodcutter_hurt_left/Woodcutter_hurt_left3.png').convert_alpha(), (50, 50)),
+        woodcutter_hurt_left3_p).convert_alpha(), (50, 50)),
 ]
 # --------------------------------------------- Интерфейс --------------------------------------------------------------
 
 # Иконка яблока --------------------------------------------------------------------------------------------------------
-apple_icon = pygame.transform.scale(pygame.image.load('Images/Interface/apple_icon.png').convert_alpha(), (30, 30))
+apple_icon_p = f.resource_path('Images/Interface/apple_icon.png')
+apple_icon = pygame.transform.scale(pygame.image.load(apple_icon_p).convert_alpha(), (30, 30))
 
 # Иконка древесины -----------------------------------------------------------------------------------------------------
-wood_icon = pygame.transform.scale(pygame.image.load('Images/Interface/drevesina_icon.png').convert_alpha(), (30, 30))
+wood_icon_p = f.resource_path('Images/Interface/drevesina_icon.png')
+wood_icon = pygame.transform.scale(pygame.image.load(wood_icon_p).convert_alpha(), (30, 30))
 
 # Иконка кокоса --------------------------------------------------------------------------------------------------------
-coconut_icon = pygame.transform.scale(pygame.image.load('Images/Interface/coconut_icon.png').convert_alpha(), (30, 30))
+coconut_icon_p = f.resource_path('Images/Interface/coconut_icon.png')
+coconut_icon = pygame.transform.scale(pygame.image.load(coconut_icon_p).convert_alpha(), (30, 30))
 
 # Иконка шишки ---------------------------------------------------------------------------------------------------------
-shishka_icon = pygame.transform.scale(pygame.image.load('Images/Interface/shishka_icon.png').convert_alpha(), (30, 30))
+shishka_icon_p = f.resource_path('Images/Interface/shishka_icon.png')
+shishka_icon = pygame.transform.scale(pygame.image.load(shishka_icon_p).convert_alpha(), (30, 30))
 
 # Окно You Died --------------------------------------------------------------------------------------------------------
-die_bg = pygame.transform.scale(pygame.image.load("Images/backgrounds/Vic.jpg").convert_alpha(), (1920, 1080))
+die_bg_p = f.resource_path("Images/backgrounds/Vic.jpg")
+die_bg = pygame.transform.scale(pygame.image.load(die_bg_p).convert_alpha(), (1920, 1080))
 
 # Фон при победе -------------------------------------------------------------------------------------------------------
-vic_bg = pygame.transform.scale(pygame.image.load('Images/backgrounds/blue_vic_bg.jpg').convert_alpha(), (1920, 1080))
+vic_bg_p = f.resource_path('Images/backgrounds/blue_vic_bg.jpg')
+vic_bg = pygame.transform.scale(pygame.image.load(vic_bg_p).convert_alpha(), (1920, 1080))
