81cdc001e57d3abaf6f38a29e992e41f50b5f06a
HodorP <hagberry_am@list.ru>
2024-02-23 14:08:56 +0400

    adapted for linux

--- God-of-Axe/sounds.py
+++ God-of-Axe/sounds.py
@@ -11,25 +11,25 @@ play_music_p = f.resource_path(r'Music/Bad Piggies Theme - Piano Tutorial.mp3')
 play_music = pygame.mixer.Sound(play_music_p)
 
 # Звук нажатия кнопок меню ---------------------------------------------------------------------------------------------
-click_p = f.resource_path(r'Music\zipclick.flac')
+click_p = f.resource_path(r'Music/zipclick.flac')
 click = pygame.mixer.Sound(click_p)
 
 # Звук взмаха топором --------------------------------------------------------------------------------------------------
-wave_p = f.resource_path(r'Music\energichnyiy-rezkiy-vzmah-razrezayuschiy-vozduh.mp3')
+wave_p = f.resource_path(r'Music/energichnyiy-rezkiy-vzmah-razrezayuschiy-vozduh.mp3')
 wave = pygame.mixer.Sound(wave_p)
 
 # Звук шага ------------------------------------------------------------------------------------------------------------
-step_p = pygame.mixer.Sound(r'Music\korotkiy-byistryiy-shag-po-lesu.mp3')
+step_p = pygame.mixer.Sound(r'Music/korotkiy-byistryiy-shag-po-lesu.mp3')
 
 # Звук удара о дерево --------------------------------------------------------------------------------------------------
-hit_tree_p = f.resource_path(r'Music\korotkiy-chtkiy-rezkiy-udar-po-derevu.mp3')
+hit_tree_p = f.resource_path(r'Music/korotkiy-chtkiy-rezkiy-udar-po-derevu.mp3')
 hit_tree = pygame.mixer.Sound(hit_tree_p)
 
 # Звуки получения урона ------------------------------------------------------------------------------------------------
-take_dmg1_p = f.resource_path(r'Music\take_dmg1.mp3')
-take_dmg2_p = f.resource_path(r'Music\take_dmg2.mp3')
-take_dmg3_p = f.resource_path(r'Music\take_dmg3.mp3')
-last_hit_p = f.resource_path(r'Music\last_hit.mp3')
+take_dmg1_p = f.resource_path(r'Music/take_dmg1.mp3')
+take_dmg2_p = f.resource_path(r'Music/take_dmg2.mp3')
+take_dmg3_p = f.resource_path(r'Music/take_dmg3.mp3')
+last_hit_p = f.resource_path(r'Music/last_hit.mp3')
 
 
 take_dmg1 = pygame.mixer.Sound(take_dmg1_p)
@@ -43,15 +43,15 @@ take_dmg_sounds_list = [take_dmg1, take_dmg2, take_dmg3]
 last_hit = pygame.mixer.Sound(last_hit_p)
 
 # Звук удара по монстру ------------------------------------------------------------------------------------------------
-hit_monster_p = f.resource_path(r'Music\rassechenie-ploti-boevym-toporom-48.mp3')
+hit_monster_p = f.resource_path(r'Music/rassechenie-ploti-boevym-toporom-48.mp3')
 hit_monster = pygame.mixer.Sound(hit_monster_p)
 
 # Звук падающего дерева ------------------------------------------------------------------------------------------------
-falling_tree_p = f.resource_path(r'Music\falling_tree.mp3')
+falling_tree_p = f.resource_path(r'Music/falling_tree.mp3')
 falling_tree = pygame.mixer.Sound(falling_tree_p)
 
 # Звук агра монстров ---------------------------------------------------------------------------------------------------
-agr_monster_p = f.resource_path(r'Music\monster-grunt_zksbhlv_.mp3')
+agr_monster_p = f.resource_path(r'Music/monster-grunt_zksbhlv_.mp3')
 agr_monster = pygame.mixer.Sound(agr_monster_p)
 
 # Звук поедания яблока -------------------------------------------------------------------------------------------------
